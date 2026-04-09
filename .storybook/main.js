ac6e99fcfbd8a91894f053bcd9bf080d710205ee
Kyrylo Osadchuk <kirill@oski.site>
2026-04-09 16:44:09 -0400

    chore: bump to 3.1.0, fix Storybook build, add @storybook/test

--- .storybook/main.js
+++ .storybook/main.js
@@ -12,6 +12,29 @@ module.exports = {
       ...config.resolve.alias,
       rt: path.resolve(__dirname, '../src'),
     }
+
+    // Ensure babel-loader handles all JS/JSX including stories and src
+    config.module.rules.push({
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['@babel/preset-env', {targets: {esmodules: true}}],
+            ['@babel/preset-react', {runtime: 'automatic'}],
+          ],
+          plugins: [
+            '@babel/plugin-transform-template-literals',
+            '@babel/plugin-proposal-class-properties',
+            '@babel/plugin-transform-async-to-generator',
+            '@babel/plugin-transform-runtime',
+            ['module-resolver', {alias: {rt: path.resolve(__dirname, '../src')}}],
+          ],
+        },
+      },
+    })
+
     return config
   },
 }
